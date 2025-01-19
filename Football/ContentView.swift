ff2161fdb3c612fa2e17518d2ceb5b907bc94e55
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-18 22:31:41 -0300

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Christian Manzaraz on 18/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Christian Manzaraz on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
